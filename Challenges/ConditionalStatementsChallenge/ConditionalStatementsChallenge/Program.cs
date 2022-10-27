8ffd70bf7fe2e1d86fac53a7fc907518c2d0f253
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-10-27 13:33:18 -0700

    Added Conditional Statements Challenge

---
+++ Challenges/ConditionalStatementsChallenge/ConditionalStatementsChallenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConditionalStatementsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            if (x > 10)
+            {
+                Console.WriteLine("X is greater than 10");
+            }
+            else if (x < 10)
+            {
+                Console.WriteLine("X is less than 10");
+            }
+            else
+            {
+                Console.WriteLine("X is equal to 10");
+            }
+            Console.ReadLine();
+        }
+    }
+}
