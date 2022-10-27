e8a43085a1c931df4693501415c681dd3adfb46b
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-10-27 13:38:40 -0700

    Added Operator Challenge

---
+++ Challenges/OperatorChallenge/OperatorChallenge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OperatorChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            if (x != 5)
+            {
+                Console.WriteLine("X does not equal 5");
+            }
+            Console.ReadLine();
+        }
+    }
+}
