a9456df021700a7f4cdf802979fca6418c7d14a9
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-10-27 13:44:05 -0700

    Added Console Output Challenge

---
+++ Challenges/ConsoleOutputChallenge/ConsoleOutputChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleOutputChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 15;
+
+            if (x > 5)
+            {
+                Console.WriteLine("X is greater than 5");
+            }
+            if (x <= 15)
+            {
+                Console.WriteLine("X is lesser than or equal to 15");
+            }
+            Console.ReadLine();
+        }
+    }
+}
