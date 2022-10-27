ce869dec0a6d659de0328eee00a1370e41293d7a
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-10-27 14:49:22 -0700

    Added Ternary Operator Challenge

---
+++ Challenges/TernaryOperatorChallenge/TernaryOperatorChallenge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TernaryOperatorChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+
+            string result = x > y ? "X is greater than Y" : "X is not greater than Y";
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
