c24bd5f7785a19992e85b7c234100ff1a46cb734
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-10-31 15:20:36 -0700

    Updated Array Exercise

--- General/ArrayExercise/ArrayExercise/Program.cs
+++ General/ArrayExercise/ArrayExercise/Program.cs
@@ -11,6 +11,7 @@ namespace ArrayExercise
         static void Main(string[] args)
         {
             // Part 1
+
             //Creating array of strings
             string[] stringArray = { "First: ", "Second: ", "Third: ", "Fourth: ", "Fifth: " };
 
@@ -34,6 +35,7 @@ namespace ArrayExercise
 
             //////////////////////////////////////////////////////////////////////////
             // Part 2
+
             //Creating infinite loop by having condition always be true
 
             //for (int i = 0; i >= 0; i++)
@@ -52,6 +54,7 @@ namespace ArrayExercise
 
             //////////////////////////////////////////////////////////////////////////
             // Part 3
+
             // Loop with  "<" for comparison
             for (int i = 0; i < 10; i++)
             {
@@ -70,6 +73,7 @@ namespace ArrayExercise
 
             //////////////////////////////////////////////////////////////////////////
             // Part 4
+
             // Creating list of strings
             List<string> colors = new List<string> { "red", "orange", "yellow", "green", "blue", "indigo", "violet" };
 
@@ -103,6 +107,67 @@ namespace ArrayExercise
                             listInput = Console.ReadLine().ToLower();
                         }
                     }
+                }
+            }
+
+            Console.ReadLine();
+
+            //////////////////////////////////////////////////////////////////////////
+            // Part 5
+
+            List<string> fruits = new List<string> { "apple", "banana", "grape", "orange", "strawberry", "banana", "mango" };
+
+            Console.WriteLine("\nPlease input text to search for in the list of fruits: ");
+            string fruitInput = Console.ReadLine().ToLower();
+            bool fruitPresent = false;
+
+            while (!fruitPresent)
+            {
+                // Using for loop to more easily track indices of duplicate items
+                for (var i = 0; i < fruits.Count; i++)
+                {
+                    if (fruits[i].Contains(fruitInput))
+                    {
+                        Console.WriteLine("\nMatching text is found at index: " + i);
+                        fruitPresent = true;
+                    }
+                    else
+                    {
+                        // Returns no match if on last iteration and fruitPresent is false,
+                        // since if a match was made fruitPresent would be true
+                        if (i == 6 && fruitPresent == false)
+                        {
+                            Console.WriteLine("\nNo matching text is found in the list.");
+                            fruitPresent = true;
+                        }
+                    }
+                }
+            }
+
+            Console.ReadLine();
+
+            //////////////////////////////////////////////////////////////////////////
+            // Part 6
+
+            // Creating actual list to be checked
+            List<string> letters = new List<string> { "A", "B", "C", "D", "B" };
+            // Creating empty list to add letters to later
+            List<string> emptyList = new List<string>();
+
+            foreach (string letter in letters)
+            {
+                // Checks emptyList - if it doesn't contain the current letter being
+                // checked, it's unique
+                if (!emptyList.Contains(letter))
+                {
+                    Console.WriteLine(letter + " = This item is unique.");
+                    // Adds the letter to emptyList because if it appears again it
+                    // will no longer be unique
+                    emptyList.Add(letter);
+                }
+                else if (emptyList.Contains(letter))
+                {
+                    Console.WriteLine(letter + " - This item is not unique.");
                 }
             }
 
