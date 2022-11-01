e1a991363a5a584c947f1b824eca9c5450181275
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-11-01 13:00:26 -0700

    Updated TwentyOne and added assignment

---
+++ General/CallingMethodsAssignment/CallingMethodsAssignment/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class Math
+    {
+        // Methods take in and return an integer, and have a userInput parameter
+        public static int Add(int userInput)
+        {
+            // Takes the userInput, does a math operation, reassigns it to new value,
+            // and returns the value to Program.cs
+            userInput = userInput + 5;
+            return userInput;
+        }
+
+        public static int Subtract(int userInput)
+        {
+            userInput = userInput - 1;
+            return userInput;
+        }
+
+        public static int Multiply(int userInput)
+        {
+            userInput = userInput * 3;
+            return userInput;
+        }
+    }
+}
---
+++ General/CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like to do addition on?");
+            // Converts the user input to an integer and assigns it to userInput
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // Calls method and reassigns userInput with updated value
+            userInput = Math.Add(userInput);
+
+            // Displays updated value to user
+            Console.WriteLine("Your number + 5 = " + userInput);
+
+            /////////////////////////////////////////////////////////
+
+            // Additional math operations (same process as above)
+            Console.WriteLine("\nPlease provide another number to do subtraction on.");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            userInput = Math.Subtract(userInput);
+
+            Console.WriteLine("Your number - 1 = " + userInput);
+
+            /////////////////////////////////////////////////////////
+
+            Console.WriteLine("\nPlease provide another number to do multiplication on.");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            userInput = Math.Multiply(userInput);
+
+            Console.WriteLine("Your number * 3 = " + userInput);
+
+            Console.ReadLine();
+        }
+    }
+}
