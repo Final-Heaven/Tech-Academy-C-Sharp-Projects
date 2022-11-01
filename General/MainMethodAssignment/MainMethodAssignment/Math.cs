f406878856fec7ff12c2b2ed57f390b3273b355d
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-11-01 14:49:06 -0700

    Added MainMethodAssignment

---
+++ General/MainMethodAssignment/MainMethodAssignment/Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Math
+    {
+        // Takes in an integer and returns an integer
+        public int Add(int number)
+        {
+            number = number + 10;
+            return number;
+        }
+
+        // Takes in a double(decimal) and returns an integer
+        // This will result in rounding
+        public int Add(double number2)
+        {
+            number2 = number2 + 5;
+            return Convert.ToInt32(number2);
+        }
+
+        // Takes in a string and returns an integer
+        // Will only work if conversion is possible
+        public int Add(string text)
+        {
+            int convert = Convert.ToInt32(text);
+            return convert + 12;
+        }
+    }
+}
---
+++ General/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the class
+            Math math1 = new Math();
+            Console.WriteLine("Please input an integer: ");
+            // Converting user input to an integer
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            // Calling method and displaying result
+            Console.WriteLine("Your number + 10 = " + math1.Add(userInput));
+
+            Console.ReadLine();
+
+            /////////////////////////////////////////////////////////////
+
+            Math math2 = new Math();
+            Console.WriteLine("Please input a decimal: ");
+            // Converting user input to a double(decimal)
+            double userInput2 = Convert.ToDouble(Console.ReadLine());
+            // Calling method and displaying result
+            Console.WriteLine("Your number + 5 = " + math2.Add(userInput2));
+
+            Console.ReadLine();
+
+            /////////////////////////////////////////////////////////////
+
+            Math math3 = new Math();
+            Console.WriteLine("Please input a number: ");
+            // Keeping user input as a string
+            // Conversion is done when the method is called
+            string userInput3 = Console.ReadLine();
+            // Calling method and displaying result
+            Console.Write("Your number + 12 = " + math3.Add(userInput3));
+
+            Console.ReadLine();
+        }
+    }
+}
