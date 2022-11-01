d0068800476ece75202ebd9839589de1b2276336
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-11-01 15:56:12 -0700

    Added MethodClassAssignment

---
+++ General/MethodClassAssignment/MethodClassAssignment/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class Math
+    {
+        // Takes two integers as parameters
+        public void Add(int userNum1, int userNum2)
+        {
+            // Does math operation on the first number
+            int result = userNum1 + 10;
+            Console.WriteLine("First number + 10 = " + result);
+            // Displays second number as is
+            Console.WriteLine("Here is the second number: " + userNum2);
+            return;
+        }
+    }
+}
---
+++ General/MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the class
+            Math math = new Math();
+
+            // Using these numbers for demonstration purposes
+            Console.WriteLine("The two integers are 5 and 7.");
+            // Calling the method and passing in two numbers
+            math.Add(5, 7);
+
+            Console.WriteLine("\nThe two new integers are 13 and 44.");
+            // Specifying parameters by name
+            math.Add(userNum1: 13, userNum2: 44);
+
+            Console.ReadLine();
+        }
+    }
+}
