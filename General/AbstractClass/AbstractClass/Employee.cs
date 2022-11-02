0548908c8a35845a18a6a6e5ca5e14cc2091b2a7
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-11-02 13:24:35 -0700

    Added AbstractClass

---
+++ General/AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class Employee : Person
+    {
+        // Overriding SayName() method from Person class
+        // Allows employee objects access to this method and any changes to it here
+        public override void SayName()
+        {
+            base.SayName();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ General/AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating an Employee object with initial values
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+
+            // Calling method
+            employee.SayName();
+        }
+    }
+}
