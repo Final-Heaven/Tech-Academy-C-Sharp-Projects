48e93a9ad7785abc072b6c811a7df0d8d45a24b7
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-11-02 12:25:42 -0700

    Added MethodsAndObjectsAssignment

---
+++ General/MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Person
+    {
+        // Two properties with data type string
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Writes person's full name to the console
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ General/MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiaing an Employee object and initializing with values
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            // Calling the superclass method
+            employee.SayName();
+        }
+    }
+}
