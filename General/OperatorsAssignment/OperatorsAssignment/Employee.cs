cd5c1ee30838ad5fdc143467805b4ce06d3b0302
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-11-02 17:06:21 -0700

    Added OperatorsAssignment

---
+++ General/OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        // Id, FirstName, and LastName properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overloading the == operator
+        // Returns boolean value to display to user
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Overloading the != operator
+        // Returns boolean value to display to user
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id != employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ General/OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating three employee objects for demonstration purposes
+            // They each have different Id values
+            Employee employeeA = new Employee() { Id = 1 };
+            Employee employeeB = new Employee() { Id = 2 };
+            Employee employeeC = new Employee() { Id = 3 };
+
+            // Comparing Id values and storing result in variable
+            bool result1 = employeeA == employeeB;
+            bool result2 = employeeC == employeeC;
+            bool result3 = employeeA != employeeB;
+
+            // Displaying results
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+
+            Console.ReadLine();
+        }
+    }
+}
