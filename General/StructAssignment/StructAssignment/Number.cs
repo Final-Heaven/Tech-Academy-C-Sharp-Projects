8b3cf57392840ce3a8dc8969ec559260cc778c29
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-11-03 15:54:36 -0700

    Added StructAssignment

---
+++ General/StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    // Creating struct
+    public struct Number
+    {
+        // Property of amount, data type decimal
+        public decimal Amount { get; set; }
+    }
+}
---
+++ General/StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating object and assigning amount
+            Number number = new Number() { Amount = 55.57M };
+
+            // Printing amount to console
+            Console.WriteLine(number.Amount);
+
+            Console.ReadLine();
+        }
+    }
+}
