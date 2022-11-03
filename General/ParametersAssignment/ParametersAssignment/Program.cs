78119d8f15dff1afc3d5178ea6bcf4cb098e4527
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-11-02 18:30:17 -0700

    Added ParametersAssignment

---
+++ General/ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> fruits = new List<string>() { "Apple", "Banana", "Orange" };
+
+            //Employee<string> employee = new Employee<string>() { Things = fruits };
+
+            //List<int> numbers = new List<int>() { 1, 2, 3 };
+
+            //Employee<int> employee1 = new Employee<int>() { Things = numbers };
+
+            //for (var i = 0; i < employee.Things.Count; i++)
+            //{
+            //    Console.WriteLine(fruits[i]);
+            //}
+
+            //for (var i = 0; i < employee1.Things.Count; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            //Console.ReadLine();
+
+            Employee<string> employee = new Employee<string>();
+
+            employee.Things.Add("Apple");
+        }
+    }
+}
