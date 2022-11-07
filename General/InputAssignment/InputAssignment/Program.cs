0f1f77a083d1ac26ffaf72bb37dd2803959f1904
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-11-07 15:25:08 -0800

    Added InputAssignment

---
+++ General/InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks user to input a number
+            Console.WriteLine("Please enter a number.");
+            // Converts user input to an integer
+            int number = Convert.ToInt32(Console.ReadLine());
+            // Creates new text file at specified location and writes user input to it
+            using (StreamWriter file = new StreamWriter(@"C:\Users\admin\Logs\InputAssignment.txt", true))
+            {
+                file.WriteLine(number);
+            }
+
+            // Reads text file and displays the number that was logged
+            string text = File.ReadAllText(@"C:\Users\admin\Logs\InputAssignment.txt");
+            Console.WriteLine("The logs show you typed the number: {0}", text); ;
+
+            Console.ReadLine();
+        }
+    }
+}
