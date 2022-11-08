084da79ba6ace6c7f6f994bd39aa5c1df5cdebc2
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-11-07 17:52:30 -0800

    Added ConstructorAssignment

---
+++ General/ConstructorAssignment/ConstructorAssignment/Chain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class Chain
+    {
+        // Property
+        public string ChainString;
+
+        // Chaining two constructors together
+        // Has string parameter and assigns value to property ChainString,
+        // but if nothing is passed in, uses default instead
+        public Chain() : this("Default")
+        {
+
+        }
+
+        public Chain(string ChainString)
+        {
+            this.ChainString = ChainString;
+        }
+    }
+}
---
+++ General/ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a const variable
+            const string myString = "Jupiter";
+
+            // Creating a variable using the keyword var
+            var myNumber = 5;
+
+            // Printing just for demonstration purposes
+            Console.WriteLine("Const variable is {0} and var keyword variable is {1}", myString, myNumber);
+
+            // Instantiating two objects, one with an argument and one without
+            // Demonstrates constructor chaining
+            Chain chain1 = new Chain();
+            Chain chain2 = new Chain("Hello");
+
+            // Prints to console to show it worked
+            Console.WriteLine("\n" + chain1.ChainString);
+            Console.WriteLine(chain2.ChainString);
+
+            Console.ReadLine();
+        }
+    }
+}
