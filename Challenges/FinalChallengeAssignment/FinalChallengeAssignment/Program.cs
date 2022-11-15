983081c6838b4554fbf6500b2c29dd291302d24e
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-11-14 16:03:14 -0800

    Added FinalChallengeAssignment

---
+++ Challenges/FinalChallengeAssignment/FinalChallengeAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace FinalChallengeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                Console.WriteLine("Enter the new student's first name: ");
+                var fName = Console.ReadLine();
+
+                Console.WriteLine("Enter the new student's last name: ");
+                var lName = Console.ReadLine();
+
+                var student = new Student { FirstName = fName, LastName = lName };
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                var query = from b in db.Students
+                            select b;
+
+                Console.WriteLine("All students in the database: ");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.FirstName + " " + item.LastName);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Challenges/FinalChallengeAssignment/FinalChallengeAssignment/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+
+namespace FinalChallengeAssignment
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
